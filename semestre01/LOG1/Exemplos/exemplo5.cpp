1b2de0ac3cd0a23058a1f29e973df8dd45f0993d
Felipe Freitas <felipefreitasdev@gmail.com>
2022-06-13 21:50:01 -0300

    Feito o exemplo 5 de matrizes

---
+++ semestre01/LOG1/Exemplos/exemplo5.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+void lerMatriz(int matrizA[6][4]) {
+    int linha, coluna;
+
+    for(linha = 0; linha < 6; linha++) {
+        for(coluna = 0; coluna < 4; coluna++) {
+            cout << "Elemento [" << linha << "] [" << coluna << "]: ";
+            cin >> matrizA[linha][coluna];
+        }
+    }
+}
+
+int maiorQueTrinta(int matrizA[6][4]) {
+    int linha, coluna, quantidade = 0;
+
+    for(linha = 0; linha < 6; linha++) {
+        for(coluna = 0; coluna < 4; coluna++) {
+            if(matrizA[linha][coluna] > 30) {
+                quantidade++;
+            }
+        }
+    }
+
+    return quantidade;
+}
+
+void montarMatriz(int matrizA[6][4], int matrizB[6][4]) {
+    int linha, coluna;
+
+    for(linha = 0; linha < 6; linha++) {
+        for(coluna = 0; coluna < 4; coluna++) {
+            if(matrizA[linha][coluna] != 30) {
+                matrizB[linha][coluna] = matrizA[linha][coluna];
+            } else {
+                matrizB[linha][coluna] = 0;
+            }
+        }
+    }
+}
+
+void imprimirMatriz(int matrizB[6][4]) {
+    int linha, coluna;
+
+    for(linha = 0; linha < 6; linha++) {
+        for(coluna = 0; coluna < 4; coluna++) {
+            cout << matrizB[linha][coluna] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    int matrizA[6][4], matrizB[6][4];
+    
+    lerMatriz(matrizA);
+
+    maiorQueTrinta(matrizA);
+
+    montarMatriz(matrizA, matrizB);
+
+    imprimirMatriz(matrizB);
+
+    return 0;
+}
